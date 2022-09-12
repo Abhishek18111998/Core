fd3418c6a4f7a03ce7d8cd8946f9b1836196d47b
unknown <mindc1july40@iiht.tech>
2022-09-12 14:11:55 +0530

    FastFoodRoleTable

---
+++ RoleModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastFoodFranchis.Model
+{
+    public class RoleModel
+    {
+        [Key]
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
